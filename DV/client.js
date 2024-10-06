9c69b0bf361e98670c1db7b47ee2df9baf0ea610
BIOLOGY SCIENCE <shiva.science10@gmail.com>
2024-10-06 21:00:11 +0530

    basic computes done

--- DV/client.js
+++ DV/client.js
@@ -1,5 +1,3 @@
-const { derivative, lusolve, parse, rationalize, evaluate } = math;
-
 const height = 250;
 const width = 500;
 const ymax = 100;
@@ -7,7 +5,7 @@ const ymin = 0;
 const timeInterval = 50;
 const maxDataPoints = 100;
 
-let totalLoad = parseFloat(document.getElementById('totalLoad').value);
+// graph sizes need to change
 
 const graph = new Rickshaw.Graph(
 {
@@ -20,12 +18,12 @@ const graph = new Rickshaw.Graph(
     series: new Rickshaw.Series.FixedDuration(
     [
         {
-            name: "one",
+            name: 'P1',
             color: "#EC644B"
         },
 
         {
-            name: "two",
+            name: 'P2',
             color: "#000000"
         }
     ],
@@ -56,93 +54,38 @@ new Rickshaw.Graph.Axis.X(
     width
 });
 
-const globalValues = {P1MAX: 200, P2MAX: 200};
+let F1, F2, P1MIN, P1MAX, P2MIN, P2MAX, totalLoad, int;
 
-function redoCalculations({P1Value, P2Value})
-{
-    const P1 = 'P1';
-    const P2 = 'P2';
-    const F1 = `8*${P1} + 0.024*${P1}^2 + 80`;
-    const F2 = `6*${P2} + 0.04*${P2}^2 + 120`;
-    
-    const df1 = derivative(F1, P1).toString();
-    const df2 = derivative(F2, P2).toString();
-    
-    // eqt in format => p1 + p2 + l = const
-
-    if (P1Value === undefined)
-    {
-        globalValues.P2 = P2Value;
-        globalValues.lambda = evaluate(df2, {P2: P2Value});
-        globalValues.P1 = totalLoad - P2Value;
-    }
-
-    if (P2Value === undefined)
-    {
-        globalValues.P1 = P1Value;
-        globalValues.lambda = evaluate(df1, {P1: P1Value});
-        globalValues.P2 = totalLoad - P1Value;
-    }
-
-    constraintCheck();
-};
-
-function constraintCheck()
-{
-    if (globalValues.P1 === 0 && globalValues.P2 === 0) globalValues.lambda = 0;
-    if (globalValues.P1 < 0) redoCalculations({P1Value: 0});
-    if (globalValues.P2 < 0) redoCalculations({P2Value: 0});
-    if (globalValues.P1 > globalValues.P1MAX) redoCalculations({P1Value: globalValues.P1MAX});
-    if (globalValues.P2 > globalValues.P2MAX) redoCalculations({P2Value: globalValues.P2MAX});
-};
-
-function calculateData()
-{
-    if (totalLoad > (globalValues.P1MAX + globalValues.P2MAX)) return;
-
-    const P1 = 'P1';
-    const P2 = 'P2';
-    const F1 = `8*${P1} + 0.024*${P1}^2 + 80`;
-    const F2 = `6*${P2} + 0.04*${P2}^2 + 120`;
-    
-    const df1 = derivative(F1, P1).toString();
-    const df2 = derivative(F2, P2).toString();
-    
-    // eqt in format => p1 + p2 + l = const
-    
-    const eq1 = rationalize(df1, true).coefficients;
-    const eq2 = rationalize(df2, true).coefficients;
-    
-    const constants = [-1 * eq1[0], -1 * eq2[0], totalLoad];
-    
-    const coefficients = [ [eq1[1], 0, -1], [0, eq2[1], -1], [1, 1, 0] ];
-    
-    const solutions = lusolve(coefficients, constants);
-    
-    globalValues.P1 = parseFloat(solutions[0][0].toFixed(4));
-    globalValues.P2 = parseFloat(solutions[1][0].toFixed(4));
-    globalValues.lambda = parseFloat(solutions[2][0].toFixed(4));
-
-    constraintCheck();
-
-    return globalValues;
-};
+const solver = new LoadDispatch();
 
 function insertRandomDatapoints()
 {
-    calculateData();
+    const { P1, P2, lambda } = solver.solve();
 
-    const { P1, P2 } = globalValues;
+    document.getElementById('P1').innerHTML = P1;
+    document.getElementById('P2').innerHTML = P2;
+    document.getElementById('lambda').innerHTML = lambda;
 
-    graph.series.addData({ one: P1, two: P2 });
-    // graph.series.addData({ one: Math.floor(Math.random() * (ymax/2)) + 10, two: (ymax + 50) / 2 });
+    graph.series.addData({ P1, P2 });
+
     graph.render();
 };
 
-
-setInterval(insertRandomDatapoints, timeInterval);
-
 document.querySelector('button').addEventListener('click', () =>
 {
-    totalLoad = parseFloat(document.getElementById('totalLoad').value);  
+    F1 = document.getElementById('F1').value;
+    F2 = document.getElementById('F2').value;
+    
+    P1MIN = parseFloat(document.getElementById('P1MIN').value);
+    P1MAX = parseFloat(document.getElementById('P1MAX').value);
+    P2MIN = parseFloat(document.getElementById('P2MIN').value);
+    P2MAX = parseFloat(document.getElementById('P2MAX').value);
+    
+    totalLoad = parseFloat(document.getElementById('totalLoad').value);
+
+    solver.setCostFunctions({ F1, F2 }).setLimits({ P1MIN, P1MAX, P2MIN, P2MAX }).setLoad(totalLoad);
+
+    if (int !== undefined) return;
+
+    int = setInterval(insertRandomDatapoints, timeInterval);
 });
---
+++ DV/compute.js
@@ -0,0 +1,108 @@
+const { derivative, lusolve, rationalize, evaluate } = math;
+
+class LoadDispatch
+{
+    F1;
+    F2;
+    dF1;
+    dF2;
+    coef1;
+    coef2;
+    P1MIN;
+    P1MAX;
+    P2MIN;
+    P2MAX;
+    totalLoad;
+    values = {};
+
+    constructor()
+    {
+        return this;
+    };
+
+    setCostFunctions({F1, F2})
+    {
+        if (!F1) this.F1 = F1;
+        if (!F2) this.F2 = F2;
+        
+        return this;
+    };
+    
+    setLimits({P1MIN, P1MAX, P2MIN, P2MAX})
+    {
+        if (!P1MIN) this.P1MIN = P1MIN;
+        if (!P1MAX) this.P1MAX = P1MAX;
+        if (!P2MIN) this.P2MIN = P2MIN;
+        if (!P2MAX) this.P2MAX = P2MAX;
+        
+        return this;
+    };
+
+    setLoad(load)
+    {
+        this.totalLoad = load;
+
+        return this;
+    };
+
+    #updateDerivative()
+    {
+        this.dF1 = derivative(F1, 'P1').toString();
+        this.dF2 = derivative(F2, 'P2').toString();
+    };
+
+    #updateCoefficients()
+    {
+        this.coef1 = rationalize(this.dF1, true).coefficients;
+        this.coef2 = rationalize(this.dF2, true).coefficients;
+    };
+
+    #redoCalculations({P1Value, P2Value})
+    {
+        this.#updateDerivative();
+
+        if (P1Value !== undefined)
+        {
+            this.values.P1 = P1Value;
+            this.values.lambda = evaluate(this.dF1, {P1: P1Value});
+            this.values.P2 = this.totalLoad - P1Value;
+        }
+
+        if (P2Value !== undefined)
+        {
+            this.values.P2 = P2Value;
+            this.values.lambda = evaluate(this.dF2, {P2: P2Value});
+            this.values.P1 = this.totalLoad - P2Value;
+        }
+
+        this.#constraintCheck();
+    };
+
+    #constraintCheck()
+    {
+        if (this.values.P1 === 0 && this.values.P2 === 0) this.values.lambda = 0;
+        if (this.values.P1 < 0) this.#redoCalculations({P1Value: 0});
+        if (this.values.P2 < 0) this.#redoCalculations({P2Value: 0});
+        if (this.values.P1 > this.P1MAX) this.#redoCalculations({P1Value: this.P1MAX});
+        if (this.values.P2 > this.P2MAX) this.#redoCalculations({P2Value: this.P2MAX});
+    };
+
+    solve()
+    {
+        this.#updateDerivative();
+        this.#updateCoefficients();
+
+        const constants = [ -1 * this.coef1[0], -1 * this.coef2[0], this.totalLoad ];
+        const coefficients = [ [this.coef1[1], 0, -1], [0, this.coef2[1], -1], [1, 1, 0] ];
+
+        const solutions = lusolve(coefficients, constants);
+
+        this.values.P1 = parseFloat(solutions[0][0].toFixed(4));
+        this.values.P2 = parseFloat(solutions[1][0].toFixed(4));
+        this.values.lambda = parseFloat(solutions[2][0].toFixed(4));
+
+        this.#constraintCheck();
+
+        return this.values;
+    };
+};
